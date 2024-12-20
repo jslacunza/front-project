dd14b2ffee6deed9f87acda2e35615c8de70e060
Jose Lacunza <103453835+jslacunza@users.noreply.github.com>
2024-12-20 09:22:27 -0300

    feat: envs file

--- src/utils/fetchGames.ts
+++ src/utils/fetchGames.ts
@@ -1,7 +1,7 @@
 import { GameResponse } from "./interfaces/GameData"
 
 export const getGames = async (page?: number): Promise<GameResponse> => {
-  const url = `http://localhost:3000/api/games${page ? '?page=' + page : '?page=1'}`
+  const url = `${process.env.NEXT_PUBLIC_GAMES_API}/api/games${page ? '?page=' + page : '?page=1'}`
   const dataFetch = await fetch(url)
   return dataFetch.json()
 }
