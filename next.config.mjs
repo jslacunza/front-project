6d61c6dc3a8f131dc6fbd28ce7630006b5d3b35d
Jose Lacunza <103453835+jslacunza@users.noreply.github.com>
2024-12-20 14:22:10 -0300

    fix: config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    swcLoader: true,
+    swcMinify: true,
+  },
+};
 
 export default nextConfig;
